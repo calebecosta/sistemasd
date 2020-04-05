d711ae3b806eb56a29d367c4d74b6b16dea9babd
Calebe <calebekosta@gmail.com>
2020-04-05 15:05:00 -0300

    commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let express = require("express");
+let app = express();
+let cors = require("cors");
+const axios = require('axios').default;
+
+app.use(cors());
+
+var port = process.env.PORT || 3000;
+app.listen(port,"0.0.0.0",function(){
+    console.log("aaa")
+
+})
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+let token;
+app.use(bodyParser.json())
+
+
+
+
