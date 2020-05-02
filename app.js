e0943ea613e34f8538e20a3bf66cf8ab85acab9f
Calebe <calebe@mac.local>
2020-05-02 20:37:48 -0300

    alteracoes

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 let express = require("express");
 let cors = require("cors");
 let app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var cifrar = require('ciphervgnr');
 
 app.use(cors());
 app.use(express.static('public'));
 
-let key = 'pesadomano';
+app.listen(PORT,function(){
+  console.log("rodando")
 
-// cifra DE Vigenère UTILIZADA. Pacote :https://www.npmjs.com/package/ciphervgnr
+})
 
 var port = process.env.PORT || 3000;
 
@@ -18,29 +16,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.on('chat message', function (mensagem) {
-    var cifrada = cifrar(mensagem, key);
-    var descriptografada = cifrar(cifrada, key, true);
-    //descomentar abaixo para testar o erro (padrao é dar certo)
-   mensagem='a';
-    
-    io.emit('message', "O servidor recebeu " + cifrada + " e a descriptografou. Resultado : " + descriptografada + ". O servidor enviou " + descriptografada + " para o cliente");
-    
-    if (mensagem ==descriptografada) {
-      console.log("mensagem :"+mensagem)
-      console.log("des: "+descriptografada)
-      io.emit('success', " Cliente  recebeu a mesma mensagem que é : " + descriptografada);
-    } else {
-      io.emit('error', "Cliente nao recebeu a mesma mensagem. Recebeu: " + mensagem);
-    }
 
-  });
-});
-
-http.listen(port, function () {
-  console.log("Acesse em: "+"http://localhost:"+ port);
-});
 
 
 
