9b8895642cc0fb14c80372967056851128d0ae46
Calebe <calebe@mac.local>
2020-05-17 19:55:43 -0300

    commit do chat alterado ok, com nova interface

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ let app = express();
 const bodyParser = require('body-parser')
 app.use(cors());
 app.use(express.static('public'));
+const api = require('./controller/apicontroller')
 
-global.armazenamento;
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,33 +15,25 @@ app.use(bodyParser.json())
 
 var port = process.env.PORT || 3000;
 
-app.listen(port,"0.0.0.0",function(){
-  
-  console.log("rodando")
+app.listen(port, "0.0.0.0", function () {
+  console.log(`Chat Rodando na porta ${port}`)
+  console.log(`Acesse http://localhost:${port}/chat`)
 })
 
-app.get('/cliente', function (req, res) {
-
-  res.sendFile(__dirname + '/usuario.html');
+app.get('/chat', function (req, res) {
+  res.sendFile(__dirname + '/chat.html');
 });
 
-
-app.post('/cliente/mensagens/enviar/', function(req,res){
+app.post('/cliente/mensagens/enviar/', function (req, res) {
+  api.gravar_mensagem(req.body)
   res.status(200);
-  res.json({ "ok": "ok" })
-    global.armazenamento = req.body;
-  //console.log(global.armazenamento)
+  res.json({ "message": "gravada" })
 })
 
-app.get('/cliente/mensagens/checar/', function(req,res){
-  if(global.armazenamento){
-   res.json(JSON.stringify(global.armazenamento));
-  }
+app.get('/cliente/mensagens/checar/', function (req, res) {
+  api.listar(function (callback) {
+    res.send(callback);
+  })
   res.status(200);
-
-
-  console.log(global.armazenamento)
-
- // console.log("Usuario que veio:" + res.body.usuario);
 })
 
--- controller/apicontroller.js
+++ controller/apicontroller.js
@@ -1,67 +1,18 @@
-const axios = require('axios').default;
-const request = require('request');
-let Usuario = require('../models/dados');
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: 'msg.db', autoload: true });
 
-exports.logar = function(dados,callback){
-    request({
-        url: "https://api.milvus.com.br/api/login/",
-        method: "POST",
-        json: dados,
-        headers: {
-            "content-type": "application/json"
-        }
-    }, function (err, body) {
-        if (body.statusCode == 200 && body.body.token) {
-           callback(   
-            {
-             username:dados.username,
-             success:'login feito',
-             status:body.statusCode,
-             token:body.body.token
-            }
-        );
-             
-        }else{  
-          callback({"error": body.body,status:body.statusCode});
-        }
-    });
-};
-
-    exports.list = function(callback){  
-        Usuario.find({},function(error,dados){
-            if(error){
-                callback({error: 'nao foi possivel encontrar nehum dado'})
-            }else{
-                callback(dados)
-            }
-        })
-    }
-
-    exports.achar = function(user,callback){  
-        Usuario.find(user,function(error,dados){
-            if(JSON.stringify(dados)=="[]"){
-                callback({error: 'nao existem usuarios'})
-            }else{
-                callback({success: dados});
-            }
-        })
-    }
-    let Usuario = require('./models/dados');
-
-exports.criar= function(dados,callback){
-    new Usuario({
-        'username': dados.username,
-        'token': dados.token,
-        'chatid':dados.chatid
-    }).save(function (error,dados) {
-        if (!error) {  
-       callback({message:"gravado"}) ;
-        } else {
-       callback({message:"error"});
-        }
-    })
-
+exports.gravar_mensagem = function(doc){
+    db.insert(doc, function (err, newDoc) {
+    }); 
 }
 
 
+exports.listar = function(callback){
+    db.find({})
+    .sort({ timestamp:1 }) 
+    .limit()
+    .exec (function (err, docs){
+        callback(docs);
+      })
+}
 
